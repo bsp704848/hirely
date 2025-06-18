d1ce9288aedef8051ef0c1cd3cd689d696309fba
bsp704848 <bsp704848@gmail.com>
2025-06-18 15:33:21 +0530

    added googlecallback page

---
+++ src/pages/GoogleCallback.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { useRouter } from 'vue-router'
+import { onMounted } from 'vue'
+import { useAuthStore } from '../stores/authStore'
+
+const authStore = useAuthStore()
+const router = useRouter()
+
+onMounted(() => {
+    setTimeout(async () => {
+        try {
+            await authStore.fetchUser(router)
+            router.push('/') 
+        } catch (err) {
+            router.push('/login')
+        }
+    }, 1000)
+})
+</script>
+
+<template>
+    <div class="text-center mt-20 text-xl font-medium">Logging in via Google...</div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import LoginPage from '../pages/LoginPage.vue'
 import EmployerDashboard from '../pages/EmployerDashboard.vue'
 import HomePage from '../pages/HomePage.vue'
 import JobForm from '../components/JobForm.vue'
-import JobList from '../components/JobList.vue' 
+import JobList from '../components/JobList.vue'
 import JobDetails from '../pages/JobDetails.vue'
 import AboutPage from '../pages/AboutPage.vue'
 import NotFoundPage from '../pages/NotFoundPage.vue'
@@ -13,18 +13,21 @@ import EmployerJobsPage from '../pages/EmployerJobsPage.vue'
 import JobApplicationForm from '../components/JobApplicationForm.vue'
 import AppliedJobs from '../pages/AppliedJobs.vue'
 import EmployerApplications from '../pages/EmployerApplications.vue'
+import GoogleCallback from '../pages/GoogleCallback.vue'
+import EmployerJob from '../pages/EmployerJob.vue'
 
 const routes = [
-  { path: '/',name:'HomePage', component: HomePage  },
-  { path: '/register',name:'RegisterPage', component: RegisterPage },
-  { path: '/login',name:'LoginPage', component: LoginPage },
-  { path: '/employer', name: 'EmployerDashboard', component: EmployerDashboard , meta: { requiresAuth: true, role: 'employer' }},
-  { path: '/addJob',name:'JobForm', component: JobForm , meta: { requiresAuth: true, role: 'employer' } },
-  { path: '/jobs',name: 'joblist', component: JobList },
-  { path: '/job/:id', component: JobDetails , meta: { requiresAuth: true, role: 'employee' } },
+  { path: '/', name: 'HomePage', component: HomePage },
+  { path: '/auth/google/callback',name: 'GoogleCallback', component: GoogleCallback, meta: { requiresAuth: true } },
+  { path: '/register', name: 'RegisterPage', component: RegisterPage },
+  { path: '/login', name: 'LoginPage', component: LoginPage },
+  { path: '/employer', name: 'EmployerDashboard', component: EmployerDashboard, meta: { requiresAuth: true, role: 'employer' } },
+  { path: '/addJob', name: 'JobForm', component: JobForm, meta: { requiresAuth: true, role: 'employer' } },
+  { path: '/jobs', name: 'joblist', component: JobList },
+  { path: '/job/:id', component: JobDetails, meta: { requiresAuth: true, role: 'employee' } },
   { path: '/about', component: AboutPage },
-  { path: '/EmployerJobspage', component: EmployerJobsPage , meta: { requiresAuth: true, role: 'employer' }  },
-  { path: '/employerjob/:id', name: 'EmployerJob', component: () => import('../pages/EmployerJob.vue'), meta: { requiresAuth: true, role: 'employer' } },
+  { path: '/EmployerJobspage', component: EmployerJobsPage, meta: { requiresAuth: true, role: 'employer' } },
+  { path: '/employerjob/:id', name: 'EmployerJob', component:EmployerJob , meta: { requiresAuth: true, role: 'employer' } },
   { path: '/apply/:id', name: 'JobApplication', component: JobApplicationForm },
   { path: '/applied-jobs', name: 'AppliedJobs', component: AppliedJobs },
   { path: '/applications', name: 'EmployerApplications', component: EmployerApplications },
@@ -34,19 +37,19 @@ const routes = [
 
 export const router = createRouter({
   history: createWebHistory(),
-  routes, 
+  routes,
   scrollBehavior() {
     return { top: 0 }
   }
-}) 
+})
 
 let isUserFetched = false;
 
-router.beforeEach(async(to, from, next) => {
+router.beforeEach(async (to, from, next) => {
   const authStore = useAuthStore()
   if (!isUserFetched) {
     try {
-      await authStore.fetchUser(); 
+      await authStore.fetchUser();
     } catch (error) {
 
     }
@@ -64,7 +67,7 @@ router.beforeEach(async(to, from, next) => {
     return next(role === 'employer' ? '/employer' : '/');
   }
 
- 
+
   if (to.meta.role && to.meta.role !== role) {
     return next('/')
   }
