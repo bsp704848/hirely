eb1082460fec3d0acb49c25cc7b178cc17399da6
bsp704848 <bsp704848@gmail.com>
2025-07-08 21:54:35 +0530

    modified socket.js

--- src/socket.js
+++ src/socket.js
@@ -2,6 +2,7 @@ import { io } from 'socket.io-client'
 const baseURL = import.meta.env.VITE_API_BASE_URL
 
 const socket = io(`${baseURL}`.replace('/api', ''), {
+  transports: ['websocket'],
   withCredentials: true
 })
 
